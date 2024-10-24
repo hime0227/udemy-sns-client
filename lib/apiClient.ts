0dbd55b5d7007daf8f4088b6c4d4d433e1c465a3
hime0227 <torchwood.captain51st+dev@gmail.com>
2024-10-25 00:55:38 +0900

    Update API base URL in apiClient.ts

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5050/api",
+  // baseURL: "http://localhost:5050/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
